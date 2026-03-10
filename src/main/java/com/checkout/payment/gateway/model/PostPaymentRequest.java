8664562e2bc4d7c2eb808c45ce436c2476cd5693
A.K. <algika9@gmail.com>
2026-03-11 01:08:21 +0300

    swagger example fixed

--- src/main/java/com/checkout/payment/gateway/model/PostPaymentRequest.java
+++ src/main/java/com/checkout/payment/gateway/model/PostPaymentRequest.java
@@ -6,6 +6,7 @@ import java.time.YearMonth;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -14,29 +15,35 @@ import jakarta.validation.constraints.Pattern;
 
 public class PostPaymentRequest implements Serializable {
 
+  @Schema(example = "2222405343248877")
   @JsonProperty("card_number")
   @NotBlank
   @Pattern(regexp = "^[0-9]{14,19}$", message = "Card number must be 14-19 digits")
   private String cardNumber;
 
+  @Schema(example = "4")
   @JsonProperty("expiry_month")
   @NotNull
   @Min(value = 1, message = "Expiry month must be between 1 and 12")
   @Max(value = 12, message = "Expiry month must be between 1 and 12")
   private Integer expiryMonth;
 
+  @Schema(example = "2025")
   @JsonProperty("expiry_year")
   @NotNull
   private Integer expiryYear;
 
+  @Schema(example = "GBP")
   @NotBlank
   @Pattern(regexp = "^(USD|EUR|GBP)$", message = "Currency must be one of USD, EUR, GBP")
   private String currency;
 
+  @Schema(example = "100")
   @NotNull
   @Min(value = 0, message = "Amount must be a positive integer")
   private Integer amount;
 
+  @Schema(example = "123")
   @NotBlank
   @Pattern(regexp = "^[0-9]{3,4}$", message = "CVV must be 3-4 digits")
   private String cvv;
