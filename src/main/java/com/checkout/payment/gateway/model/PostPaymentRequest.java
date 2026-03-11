5c7a67e38627c81b46ee19b8a5b47422590acceb
A.K. <algika9@gmail.com>
2026-03-12 00:41:48 +0300

    dont log cvv

--- src/main/java/com/checkout/payment/gateway/model/PostPaymentRequest.java
+++ src/main/java/com/checkout/payment/gateway/model/PostPaymentRequest.java
@@ -119,7 +119,6 @@ public class PostPaymentRequest implements Serializable {
         ", expiryYear=" + expiryYear +
         ", currency='" + currency + '\'' +
         ", amount=" + amount +
-        ", cvv=" + cvv +
         '}';
   }
 }
